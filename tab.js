e40cf37f089452763a413b969cfd628e6b6a5020
yunghsin Lee <lenalee7963@gmail.com>
2022-02-26 17:04:18 +0900

    tab for

---
+++ tab.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < $('.tab-button').length; i++) {
+  $(".tab-button")
+    .eq(i)
+    .click(function () {
+      $(".tab-button").removeClass("active");
+      $(".tab-content").removeClass("show");
+      $(".tab-button").eq(i).addClass("active");
+      $(".tab-content").eq(i).addClass("show");
+    });
+}
