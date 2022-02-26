67c725dffbd709cce5ee3f16fbf73cbff30b6930
yunghsin Lee <lenalee7963@gmail.com>
2022-02-26 17:35:27 +0900

    tab function develop

--- tab.js
+++ tab.js
@@ -1,10 +1,20 @@
-for (let i = 0; i < $('.tab-button').length; i++) {
-  $(".tab-button")
-    .eq(i)
-    .click(function () {
-      $(".tab-button").removeClass("active");
-      $(".tab-content").removeClass("show");
-      $(".tab-button").eq(i).addClass("active");
-      $(".tab-content").eq(i).addClass("show");
-    });
+// for (let i = 0; i < $('.tab-button').length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .click(function () {
+//         openTab(i);
+     
+//     });
+// }
+
+$('.list').click(function(e){
+    openTab(e.target.dataset.id);
+   
+})
+//html에 data-id 로 id 부여 후 dataset.id로 데이터 아이디 사용가능 
+function openTab(num){
+    $(".tab-button").removeClass("active");
+    $(".tab-content").removeClass("show");
+    $(".tab-button").eq(num).addClass("active");
+    $(".tab-content").eq(num).addClass("show");   
 }
